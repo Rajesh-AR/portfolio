3b079ec8a799dbffebb42856e2bab6934cc3d052
Rajesh Arya <rajesh.arya.py@gmail.com>
2022-05-22 21:44:29 +0000

    syncing the code to github

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import smtplib
+from email.message import EmailMessage
+
 app = Flask(__name__)
 
 
@@ -18,13 +21,29 @@ def store_data(data):
     with open('database.txt', mode='a') as databse:
         file = databse.write(f"\n{email}, {subject}, {message}")
 
+def send_email_confirmation(name, email_id):
+    email = EmailMessage()
+    email['from'] = 'Rajesh Arya'
+    email['to'] = email_id
+    email['subject'] = 'Contact form has been submitted !!'
+
+    email.set_content(f'Hi {name},\n\nYour contact request has been submitted and confirmed. We will get back to you as soon as possible.\nThis is an automated email, so please do not reply on this email.\n\n Regards,\n Rajesh Arya\n Python Developer ')
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('rajesh.arya.py@gmail.com', 'Imra@111')
+        smtp.send_message(email)
+
 def store_data_in_csv(data):
+    name = data["name"]
     email = data["email"]
     subject = data["subject"]
     message = data["message"]
     with open('database.csv', mode='a', newline='') as database1:
         write = csv.writer(database1)
-        write.writerow([email, subject, message])
+        write.writerow([name, email, subject, message])
+    send_email_confirmation(name, email)
 
 
 @app.route("/submit_form", methods=['GET', 'POST'])
